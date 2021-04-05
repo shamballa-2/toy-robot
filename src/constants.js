34c02c7ebb5d8cf2237b48e80e49490c13183f02
shamballa-2 <nishanksamant95@gmail.com>
2021-04-05 10:41:25 +1000

    adding constants to be used in the app

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export default {
+  directions: {
+    NORTH: 'NORTH',
+    EAST: 'EAST',
+    WEST: 'WEST',
+    SOUTH: 'SOUTH',
+  },
+  dimensions: {
+    X_MIN: 0,
+    Y_MIN: 0,
+    X_MAX: 5,
+    Y_MAX: 5,
+  },
+};
