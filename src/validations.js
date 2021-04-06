eabff84017953f60f0bfd41b1b8f2365abcdc265
shamballa-2 <nishanksamant95@gmail.com>
2021-04-07 01:38:30 +1000

    replacing validation messages as constants

--- src/validations.js
+++ src/validations.js
@@ -1,19 +1,22 @@
 import constants from './constants';
 
-const { dimensions, directions } = constants;
+const { dimensions, directions, validationMessages } = constants;
 const {
   NORTH, SOUTH, WEST, EAST,
 } = directions;
 const {
   X_MAX, X_MIN, Y_MAX, Y_MIN,
 } = dimensions;
+const {
+  X_NOT_NUMBER, Y_NOT_NUMBER, X_LIMIT, Y_LIMIT, INVALID_DIRECTION, INVALID_PLACEMENT, INVALID_PLACE_COMMAND, INVALID_PLACE_PARAMS
+} = validationMessages;
 
 // validates the horizontal position value
 export function validXDimension(value) {
   if (isNaN(value)) {
-    throw new Error('X VALUE SHOULD BE A NUMBER');
+    throw new Error(X_NOT_NUMBER);
   } else if (value < X_MIN || value >= X_MAX) {
-    throw new Error('X MAXED OUT');
+    throw new Error(X_LIMIT);
   }
   return Number(value);
 }
@@ -21,9 +24,9 @@ export function validXDimension(value) {
 // validates the vertical position value
 export function validYDimension(value) {
   if (isNaN(value)) {
-    throw new Error('Y VALUE SHOULD BE A NUMBER');
+    throw new Error(Y_NOT_NUMBER);
   } else if (value < Y_MIN || value >= Y_MAX) {
-    throw new Error('Y MAXED OUT');
+    throw new Error(Y_LIMIT);
   }
   return Number(value);
 }
@@ -31,7 +34,7 @@ export function validYDimension(value) {
 // validates the input direction
 export function validDirection(value) {
   if (![NORTH, SOUTH, WEST, EAST].includes(value)) {
-    throw new Error('INVALID DIRECTION VALUE');
+    throw new Error(INVALID_DIRECTION);
   }
   return value;
 }
@@ -39,11 +42,11 @@ export function validDirection(value) {
 // validates whether a valid place command is used
 export function validPlaceCommand(args) {
   if (!args) {
-    throw new Error('PLACE CMD NEEDS PARAMETERS');
+    throw new Error(INVALID_PLACE_COMMAND);
   } else {
     let placeArgs = args.split(',');
     if (placeArgs.length !== 3) {
-      throw new Error('PLACE CMD NEEDS 3 PARAMETERS: X,Y,F');
+      throw new Error(INVALID_PLACE_PARAMS);
     }
     return placeArgs;
   }
@@ -52,7 +55,7 @@ export function validPlaceCommand(args) {
 // validates whether the toy is placed based on its face value
 export function validToyPlacement(face) {
   if (face === '') {
-    throw new Error('TOY NEEDS TO BE PLACED FIRST');
+    throw new Error(INVALID_PLACEMENT);
   }
   return true;
 }
@@ -73,7 +76,7 @@ export function validateMove(face, xPos, yPos) {
       validXDimension(xPos - 1);
       break;
     default:
-      throw new Error('INVALID DIRECTION VALUE');
+      throw new Error(INVALID_DIRECTION);
   }
   return true;
 }
--- tests/validation.spec.js
+++ tests/validation.spec.js
@@ -2,22 +2,27 @@ import {
     validXDimension, validYDimension, validDirection, validateMove, validToyPlacement, validPlaceCommand
   } from '../src/validations';
   
+import constants from '../src/constants';
+const { validationMessages } = constants;
 
-
-  describe('position validation tests', () => {
+const {
+    X_NOT_NUMBER, Y_NOT_NUMBER, X_LIMIT, Y_LIMIT, INVALID_DIRECTION, INVALID_PLACEMENT, INVALID_PLACE_COMMAND, INVALID_PLACE_PARAMS
+  } = validationMessages;
+  
+describe('position validation tests', () => {
 
     it('should not allow non-numeric x value', () => {
 
         expect(validXDimension(2)).toBe(2);
         expect(validXDimension(4)).toBe(4);
-        expect(() => { validXDimension('asd') }).toThrow('X VALUE SHOULD BE A NUMBER');
+        expect(() => { validXDimension('asd') }).toThrow(X_NOT_NUMBER);
     });
 
     it('should not allow non-numeric y value', () => {
-        
+
         expect(validYDimension(2)).toBe(2);
         expect(validYDimension(4)).toBe(4);
-        expect(() => { validYDimension('asd') }).toThrow('Y VALUE SHOULD BE A NUMBER');
+        expect(() => { validYDimension('asd') }).toThrow(Y_NOT_NUMBER);
     });
 
 
@@ -25,14 +30,14 @@ import {
 
         expect(validXDimension(2)).toBe(2);
         expect(validXDimension(4)).toBe(4);
-        expect(() => { validXDimension(10) }).toThrow('X MAXED OUT');
+        expect(() => { validXDimension(10) }).toThrow(X_LIMIT);
     });
 
     it('should not allow value lesser than x min limit', () => {
 
         expect(validXDimension(2)).toBe(2);
         expect(validXDimension(4)).toBe(4);
-        expect(() => { validXDimension(-14) }).toThrow('X MAXED OUT');
+        expect(() => { validXDimension(-14) }).toThrow(X_LIMIT);
 
     });
 
@@ -40,7 +45,7 @@ import {
 
         expect(validYDimension(2)).toBe(2);
         expect(validYDimension(4)).toBe(4);
-        expect(() => { validYDimension(10)}).toThrow("Y MAXED OUT");
+        expect(() => { validYDimension(10)}).toThrow(Y_LIMIT);
 
     });
 
@@ -48,7 +53,7 @@ import {
 
         expect(validYDimension(2)).toBe(2);
         expect(validYDimension(4)).toBe(4);
-        expect(() => { validYDimension(-6)}).toThrow('Y MAXED OUT');
+        expect(() => { validYDimension(-6)}).toThrow(Y_LIMIT);
 
     });
 
@@ -66,10 +71,10 @@ import {
 
     it('ignores invalid direction values ', () => {
 
-        expect(() => validDirection("N")).toThrow("INVALID DIRECTION VALUE");
-        expect(() => validDirection("north")).toThrow("INVALID DIRECTION VALUE");
-        expect(() => validDirection("north-west")).toThrow("INVALID DIRECTION VALUE");
-        expect(() => validDirection("NW")).toThrow("INVALID DIRECTION VALUE");
+        expect(() => validDirection("N")).toThrow(INVALID_DIRECTION);
+        expect(() => validDirection("north")).toThrow(INVALID_DIRECTION);
+        expect(() => validDirection("north-west")).toThrow(INVALID_DIRECTION);
+        expect(() => validDirection("NW")).toThrow(INVALID_DIRECTION);
 
     });
 
@@ -87,10 +92,10 @@ import {
 
     it('rejects invalid move for NORTH facing toy robot', () => {
 
-        expect(() => validateMove("NORTH", 1, 4)).toThrow('Y MAXED OUT');
-        expect(() => validateMove("NORTH", 0, 4)).toThrow('Y MAXED OUT');
-        expect(() => validateMove("NORTH", 1, 5)).toThrow('Y MAXED OUT');
-        expect(() => validateMove("NORTH", 4, 6)).toThrow('Y MAXED OUT');
+        expect(() => validateMove("NORTH", 1, 4)).toThrow(Y_LIMIT);
+        expect(() => validateMove("NORTH", 0, 4)).toThrow(Y_LIMIT);
+        expect(() => validateMove("NORTH", 1, 5)).toThrow(Y_LIMIT);
+        expect(() => validateMove("NORTH", 4, 6)).toThrow(Y_LIMIT);
     });
 
 
@@ -104,10 +109,10 @@ import {
 
     it('rejects invalid moves for SOUTH facing toy robot', () => {
 
-        expect(() => validateMove("SOUTH", 1, 0)).toThrow('Y MAXED OUT');
-        expect(() => validateMove("SOUTH", 3, 0)).toThrow('Y MAXED OUT');
-        expect(() => validateMove("SOUTH", 1, -1)).toThrow('Y MAXED OUT');
-        expect(() => validateMove("SOUTH", 4, -3)).toThrow('Y MAXED OUT');
+        expect(() => validateMove("SOUTH", 1, 0)).toThrow(Y_LIMIT);
+        expect(() => validateMove("SOUTH", 3, 0)).toThrow(Y_LIMIT);
+        expect(() => validateMove("SOUTH", 1, -1)).toThrow(Y_LIMIT);
+        expect(() => validateMove("SOUTH", 4, -3)).toThrow(Y_LIMIT);
 
     });
 
@@ -121,10 +126,10 @@ import {
 
     it('rejects invalid moves for EAST facing toy robot', () => {
 
-        expect(() => validateMove("EAST", 4, 0)).toThrow('X MAXED OUT');
-        expect(() => validateMove("EAST", 4, 1)).toThrow('X MAXED OUT');
-        expect(() => validateMove("EAST", 5, 0)).toThrow('X MAXED OUT');
-        expect(() => validateMove("EAST", 6, 0)).toThrow('X MAXED OUT');
+        expect(() => validateMove("EAST", 4, 0)).toThrow(X_LIMIT);
+        expect(() => validateMove("EAST", 4, 1)).toThrow(X_LIMIT);
+        expect(() => validateMove("EAST", 5, 0)).toThrow(X_LIMIT);
+        expect(() => validateMove("EAST", 6, 0)).toThrow(X_LIMIT);
 
     });
 
@@ -139,19 +144,19 @@ import {
 
     it('rejects invalid moves for WEST facing toy robot', () => {
 
-        expect(() => validateMove("WEST", 0, 0)).toThrow('X MAXED OUT');
-        expect(() => validateMove("WEST", 0, 4)).toThrow('X MAXED OUT');
-        expect(() => validateMove("WEST", -1, 0)).toThrow('X MAXED OUT');
-        expect(() => validateMove("WEST", -3, 0)).toThrow('X MAXED OUT');
+        expect(() => validateMove("WEST", 0, 0)).toThrow(X_LIMIT);
+        expect(() => validateMove("WEST", 0, 4)).toThrow(X_LIMIT);
+        expect(() => validateMove("WEST", -1, 0)).toThrow(X_LIMIT);
+        expect(() => validateMove("WEST", -3, 0)).toThrow(X_LIMIT);
 
     });
 
     it('ignores move for invalid specified direction', () => {
 
-        expect(() => validateMove("W", 0, 0)).toThrow('INVALID DIRECTION VALUE');
-        expect(() => validateMove("NW", 0, 1)).toThrow('INVALID DIRECTION VALUE');
-        expect(() => validateMove("n", 1, 3)).toThrow('INVALID DIRECTION VALUE');
-        expect(() => validateMove("North", 3, 0)).toThrow('INVALID DIRECTION VALUE');
+        expect(() => validateMove("W", 0, 0)).toThrow(INVALID_DIRECTION);
+        expect(() => validateMove("NW", 0, 1)).toThrow(INVALID_DIRECTION);
+        expect(() => validateMove("n", 1, 3)).toThrow(INVALID_DIRECTION);
+        expect(() => validateMove("North", 3, 0)).toThrow(INVALID_DIRECTION);
 
     });
 
@@ -168,18 +173,18 @@ import {
     });
 
     it('ignores if toy is not facing in a direction', () => {
-        expect(() => validToyPlacement("")).toThrow('TOY NEEDS TO BE PLACED FIRST');
+        expect(() => validToyPlacement("")).toThrow(INVALID_PLACEMENT);
     });
 
     it('ignores if place command has no parmaters and shows message', () => {
-        expect(() => validPlaceCommand("")).toThrow('PLACE CMD NEEDS PARAMETERS');
-        expect(() => validPlaceCommand(undefined)).toThrow('PLACE CMD NEEDS PARAMETERS');
+        expect(() => validPlaceCommand("")).toThrow(INVALID_PLACE_COMMAND);
+        expect(() => validPlaceCommand(undefined)).toThrow(INVALID_PLACE_COMMAND);
     });
 
     it('ignores if place command fails to provide 3 parameters and shows message', () => {
-        expect(() => validPlaceCommand("2")).toThrow('PLACE CMD NEEDS 3 PARAMETERS: X,Y,F');
-        expect(() => validPlaceCommand("2,NORTH,2,3")).toThrow('PLACE CMD NEEDS 3 PARAMETERS: X,Y,F');
-        expect(() => validPlaceCommand(",,,")).toThrow('PLACE CMD NEEDS 3 PARAMETERS: X,Y,F');
+        expect(() => validPlaceCommand("2")).toThrow(INVALID_PLACE_PARAMS);
+        expect(() => validPlaceCommand("2,NORTH,2,3")).toThrow(INVALID_PLACE_PARAMS);
+        expect(() => validPlaceCommand(",,,")).toThrow(INVALID_PLACE_PARAMS);
 
     });
 
